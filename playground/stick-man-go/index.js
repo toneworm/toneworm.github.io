be0c3de28757343d68457337c0a57629f8d91ab2
Rich Williams <rich@toneworm.com>
2025-01-05 15:48:23 +0000

    Added some gradients, drop shadow stars, custom-stamp web component test

--- playground/stick-man-go/index.js
+++ playground/stick-man-go/index.js
@@ -1,7 +1,13 @@
+// dom elements
 const conveyor = document.querySelector(".conveyor");
+const stamps = document.querySelectorAll(".stamp");
+
 const word = "go";
-const animationBaseDurationSecs = 20;
+const animationBaseDurationSecs = 40;
 const animationDurationRangeSecs = 10;
+
+// change for alternate lines, otherwise they'll be random
+const alternateLines = true;
 
 function createChunks() {
   const chunks = document.createElement("div");
@@ -39,7 +45,13 @@ function createLines(num) {
       animationBaseDurationSecs +
       Math.random() * animationDurationRangeSecs +
       "s";
-    line.style.animationDirection = Math.random() > 0.5 ? "reverse" : "normal";
+    line.style.animationDirection = alternateLines
+      ? i % 2 === 0
+        ? "normal"
+        : "reverse"
+      : Math.random() > 0.5
+      ? "reverse"
+      : "normal";
 
     const chunks = createChunks();
     line.appendChild(chunks);
@@ -50,4 +62,47 @@ function createLines(num) {
 document.addEventListener("DOMContentLoaded", () => {
   // create go lines
   createLines(80);
+
+  stamps.forEach((el, key) =>
+    el.addEventListener("click", (e) => {
+      console.log(el, key);
+    })
+  );
 });
+
+// star shape web component
+class Stamp extends HTMLElement {
+  constructor() {
+    super();
+    this.className = "stamp";
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+      >
+        <path
+          d="M50 0 L61 25 L90 10 L75 39 L100 50 L75 61 L90 90 L61 75 L50 100 L39 75 L10 90 L25 61 L0 50 L25 39 L10 10 L39 25 Z"
+          fill="currentColor"
+        />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+      >
+        <path
+          d="M50 0 L61 25 L90 10 L75 39 L100 50 L75 61 L90 90 L61 75 L50 100 L39 75 L10 90 L25 61 L0 50 L25 39 L10 10 L39 25 Z"
+          fill="currentColor"
+        />
+      </svg>
+    `;
+  }
+}
+
+customElements.define("custom-stamp", Stamp);
